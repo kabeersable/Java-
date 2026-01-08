b7a4fb828be926619ca0edd0b6ff086a11e4a024
kabeersable <kabeersable007@gmail.com>
2026-01-08 17:35:34 +0530

    Java Variables

---
+++ Variables.java
@@ -0,0 +1,23 @@
+public class Variables{
+    public static void main(String[] args){
+        // Variable declaration and initialization
+        int number = 20;
+        String message = "Hello World";
+        float decimalvalue = 10.5f;
+        char character = 'A';
+        boolean isJavaFun = true;
+
+        // final variable can not be changed 
+        final int constantValue = 100;
+
+        // printing variables
+        System.out.println("this is integer:" +number);
+        System.out.println("this is String:" +message);
+        System.out.println("this is float:" +decimalvalue);
+        System.out.println("this is char:" +character);
+        System.out.println("this is booleen values isJavaFun:" +isJavaFun);
+
+        
+
+    }
+}
