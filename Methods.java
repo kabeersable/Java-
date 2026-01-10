eeba6fd7e0f9af2152b692f7b3e5ee766a32ee4f
Kabeer Sable <kabeersable007@gmail.com>
2026-01-10 17:05:16 +0530

    how to create your own Java Methods

---
+++ Methods.java
@@ -0,0 +1,13 @@
+public class Methods{
+    static void myMethod(){
+        System.out.println("this is my method testing");
+    }
+
+    public static void main (String [] args){
+
+        // calling myMethod
+
+        myMethod();
+
+    }
+}
