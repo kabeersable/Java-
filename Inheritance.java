aba86ca9302913aff885ab505a7840915d99c7ab
Kabeer Sable <kabeersable007@gmail.com>
2026-01-12 20:18:00 +0530

    understanfing modifier using inheritance

---
+++ Inheritance.java
@@ -0,0 +1,48 @@
+
+class SuperClass {
+    private int a =8;
+    public int b = 10;
+    protected int c = 20;
+
+    void display(){
+        System.out.println("only private member can be access a :"+a);
+    }
+
+    public int geta (){
+        return a; 
+    }
+}
+
+class SubClass extends SuperClass{
+
+    void display(){
+        System.out.println("Accessing Private member  in subclass using getter method a :"+geta());
+        System.out.println("we can access public member in subclass b : "+b);
+        System.out.println("Access protected member of class c:" +c);
+    }
+
+}
+
+
+
+public class Inheritance {
+
+   public static void main(String[] args) {
+    SuperClass Sc = new SuperClass();
+    SubClass sc = new SubClass();
+
+    Sc.display();
+    sc.display();
+       
+    }
+}
+
+
+
+
+    
+
+
+
+
+
